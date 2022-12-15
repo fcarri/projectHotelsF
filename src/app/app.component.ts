67b8b0091d75a0b719a45ac7a8c0ae134932ed43
fcarri <114770670+fcarri@users.noreply.github.com>
2022-12-15 09:58:21 +0100

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,9 @@ export class AppComponent{
 
     var iframe = document.createElement('iframe');
     iframe.src = 'https://my.atlistmaps.com/map/1fc716ac-6e9a-4f13-af42-c4c759dfb163?share=true;charset=utf-8,';
-
+    iframe.width='100%'
+    iframe.height='690'
+    //iframe.style.marginLeft='200px'
     document.body.appendChild(iframe);
 
   }
